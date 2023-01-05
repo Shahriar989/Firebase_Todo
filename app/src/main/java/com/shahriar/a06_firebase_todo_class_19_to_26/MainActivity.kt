1a167cf4696992e50f35976f6a072bc394fe581b
Shahriar <https://github.com/Shahriar989/E-Cart.git>
2023-01-05 22:33:34 +0600

    init

---
+++ app/src/main/java/com/shahriar/a06_firebase_todo_class_19_to_26/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.shahriar.a06_firebase_todo_class_19_to_26
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
