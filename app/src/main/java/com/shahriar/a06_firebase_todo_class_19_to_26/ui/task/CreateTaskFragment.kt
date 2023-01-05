ba03695c21d0a6ae242f4bdac8f235f80d821423
Shahriar <https://github.com/Shahriar989/E-Cart.git>
2023-01-06 00:24:39 +0600

    init

---
+++ app/src/main/java/com/shahriar/a06_firebase_todo_class_19_to_26/ui/task/CreateTaskFragment.kt
@@ -0,0 +1,19 @@
+package com.shahriar.a06_firebase_todo_class_19_to_26.ui.task
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.shahriar.a06_firebase_todo_class_19_to_26.R
+
+class CreateTaskFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_task, container, false)
+    }
+}
